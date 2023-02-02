5d899b7ae93bedef06255eb1fcb97a89400b2c80
pawanmkr <iampawanmkr@gmail.com>
2023-02-03 01:26:18 +0530

    previous files deleted

--- src/server.js
+++
@@ -1,8 +0,0 @@
-import http from 'http';
-import app from './app.js'
-
-const server = http.createServer(app);
-const PORT  = process.env.PORT || 7090;
-server.listen(PORT, () => {
-    console.log(`server runnning on http://localhost:${PORT}/`);
-});
